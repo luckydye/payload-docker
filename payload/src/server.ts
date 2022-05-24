b3f43bf3b4c3f2672b9cf74abeab27d6b0ec5431
Tim Havlicek <t.havlicek@s-v.de>
2022-05-24 17:57:40 +0200

    init

---
+++ payload/src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import payload from "payload";
+
+require("dotenv").config();
+const app = express();
+
+// Redirect root to Admin panel
+app.get("/", (_, res) => {
+  res.redirect("/admin");
+});
+
+// Initialize Payload
+payload.init({
+  secret: process.env.PAYLOAD_SECRET,
+  mongoURL: process.env.MONGODB_URI,
+  mongoOptions: {
+    user: "payload",
+    pass: "payload",
+  },
+  express: app,
+  onInit: () => {
+    payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+  },
+});
+
+// Add your own express routes here
+
+app.listen(3000);
